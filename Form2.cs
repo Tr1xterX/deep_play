00896fd0a7449633805c1f57a36bc9bddd648ccd
Tr1xterX <107270934+Tr1xterX@users.noreply.github.com>
2022-06-10 23:07:24 +0600

    Deep_play_test
    c# + DB

---
+++ Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace project_deepplay
+{
+    private SqlConnection sqlConnection = null;
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        //событие подключения 
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
